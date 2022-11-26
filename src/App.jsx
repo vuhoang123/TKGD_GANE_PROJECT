f82a1fc222bd9e91f594b0d67cc7a52a3b6c4ab9
ntanh <ku12322132@gmail.com>
2022-11-26 18:28:16 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import HomeTemplate from "./templates/HomeTemplate/HomeTemplate";
 const Home = lazy(() => import("./Pages/Home/Home"));
 const NotFound = lazy(() => import("./components/NotFound/NotFound"));
 const LandingPage = lazy(() => import("./Pages/LandingPage/LandingPage"));
+const Loading = lazy(() => import("./components/Loading/Loading"));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
+      <Suspense fallback={Loading}>
+      <Routes>
           <Route path="/" element={LandingPage} exact />
           <HomeTemplate path="/home" element={Home} exact />
           <Route path="*" element={NotFound} />
         </Routes>
+      </Suspense>
       </BrowserRouter>
     </div>
   );
