bdabfb3d08d1de104f81ca26881296712036a6e6
vuhoang123 <hoanganhvu741852963@gmail.com>
2022-11-27 02:02:58 +0700

    header,fontawesome, boostrap cdn

--- src/templates/HomeTemplate/Layout/Header/Header.jsx
+++ src/templates/HomeTemplate/Layout/Header/Header.jsx
@@ -1,10 +1,88 @@
 import React from "react";
 import "./Header.scss";
+import logo from "/public/vite.svg";
+import { NavLink } from "react-router-dom/dist";
 
 function Header() {
   return (
-  <header id="Header">header</header>
-  )
+    <header id="Header" className="sticky-top">
+      <nav className="navbar navbar-expand-lg navbar-light p-3 shadow-sm ">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={logo} />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="mx-auto my-3 d-lg-none d-sm-block d-xs-block">
+            <div className="input-group">
+              <span className="btn-search border-warning input-group-text text-white">
+                <i className=" fa-solid fa-magnifying-glass" />
+              </span>
+              <input type="text" className="form-control border-warning" />
+              <button className="btn btn-search text-white">Search</button>
+            </div>
+          </div>
+          <div className=" collapse navbar-collapse" id="navbarNavDropdown">
+            <div className="ms-auto d-none d-lg-block">
+              <div className="input-group">
+                <span className="border-warning input-group-text btn-search text-white">
+                  <i className="fa-solid fa-magnifying-glass" />
+                </span>
+                <input
+                  type="text"
+                  className="form-control border-warning"
+                  style={{ color: "#7a7a7a" }}
+                />
+                <button className="btn btn-search text-white">Search</button>
+              </div>
+            </div>
+            <ul className="navbar-nav ms-auto ">
+              <li className="nav-item">
+                <a
+                  className="nav-link mx-2 text-uppercase active"
+                  aria-current="page"
+                  href="#"
+                >
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  NEWS
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  FORUM
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  SHOP
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto ">
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  <i className="fa-solid fa-circle-user me-1" /> Account
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
