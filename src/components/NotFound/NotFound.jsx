73bf3a2d7a89b5796dd60003bf26b000b2cbe570
vuhoang123 <hoanganhvu741852963@gmail.com>
2022-11-27 19:12:22 +0700

    news flow

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import img404 from "/src/assets/image/404.svg";
+import "./NotFound.scss";
+import { Link } from "react-router-dom";
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="not-found">
+      <div className="container">
+        <div className="content">
+          <img src={img404} />
+          <p className="text">
+            Một lỗi bất thường đã xảy ra, vui lòng quay về trang chủ.
+          </p>
+          <Link to="/home ">
+            <button className="btn btn-home">Trang chủ</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
