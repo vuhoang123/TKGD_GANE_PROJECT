257d2a73deb3eb7f0270d7c066fb01f59f372fa9
vuhoang123 <hoanganhvu741852963@gmail.com>
2022-11-27 11:41:52 +0700

    footer

--- src/templates/HomeTemplate/Layout/Footer/Footer.jsx
+++ src/templates/HomeTemplate/Layout/Footer/Footer.jsx
@@ -1,10 +1,134 @@
-import React from 'react'
-import './Footer.scss'
-
+import React from "react";
+import "./Footer.scss";
+import logo from "/src/assets/image/logo.png";
+import QR from "/src/assets/image/QRcode.svg";
 function Footer() {
   return (
-    <footer id='Footer'>footer</footer>
-  )
+    <footer id="Footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <div className="logo">
+              <img src={logo} />
+              <p>Trang web tin tức và mua sắm gamming gear hàng đầu Việt Nam</p>
+            </div>
+          </div>
+          <div className="col-6 row mid-content">
+            <div className="col-3">
+              <p>Product</p>
+              <hr className="line" />
+              <ul>
+                <li>
+                  <a>Landing Page</a>
+                </li>
+                <li>
+                  <a>News</a>
+                </li>
+                <li>
+                  <a>Gamming Gear</a>
+                </li>
+                <li>
+                  <a>Content</a>
+                </li>
+                <li>
+                  <a>Community</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-3">
+              <p>Product</p>
+              <hr className="line" />
+              <ul>
+                <li>
+                  <a>Landing Page</a>
+                </li>
+                <li>
+                  <a>News</a>
+                </li>
+                <li>
+                  <a>Gamming Gear</a>
+                </li>
+                <li>
+                  <a>Content</a>
+                </li>
+                <li>
+                  <a>Community</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-3">
+              <p>Product</p>
+              <hr className="line" />
+              <ul>
+                <li>
+                  <a>Landing Page</a>
+                </li>
+                <li>
+                  <a>News</a>
+                </li>
+                <li>
+                  <a>Gamming Gear</a>
+                </li>
+                <li>
+                  <a>Content</a>
+                </li>
+                <li>
+                  <a>Community</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-3">
+              <p>Product</p>
+              <hr className="line" />
+              <ul>
+                <li>
+                  <a>Landing Page</a>
+                </li>
+                <li>
+                  <a>News</a>
+                </li>
+                <li>
+                  <a>Gamming Gear</a>
+                </li>
+                <li>
+                  <a>Content</a>
+                </li>
+                <li>
+                  <a>Community</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <hr className="vertical-line" />
+          <div className="col-3">
+            <div className=" d-flex flex-column align-items-center justyfy-content-center gap-4">
+              <img src={QR}></img>
+              <div className="d-flex gap-3">
+                <button class=" d-flex align-items-center justify-content-around btn btn-outline-light btn-icon-text btn-download px-1">
+                  <i class="fa-brands fa-apple btn-icon-prepend icon "></i>
+                  <span class="d-inline-block text-left">
+                    <small class="font-weight-light d-flex text-left">
+                      Available on the
+                    </small>
+                    App Store
+                  </span>
+                </button>
+                <button class="d-flex align-items-center justify-content-around btn btn-outline-light btn-icon-text btn-download px-1">
+                  <i class="fa-brands fa-google-play btn-icon-prepend icon"></i>
+                  <span class="d-inline-block text-left">
+                    <small class="font-weight-light d-flex text-left">
+                      Get it on
+                    </small>
+                    Google Play
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
